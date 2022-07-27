eaad4aa52536bb56c6c1dd437800dea8d9ffcae5
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-07-27 06:55:08 +0530

    Added: UC2

--- src/main/java/SnakeLadder/SnakeAndLadder/snakeLadder.java
+++ src/main/java/SnakeLadder/SnakeAndLadder/snakeLadder.java
@@ -5,10 +5,16 @@ import java.util.Random;
 public class snakeLadder {
 	public static void main(String[] args) {
 		UC1();
+		UC2();
 		
 	}
 	public static void UC1() {
 		int position =0;
 		System.out.println("Player starting Position : " + position);
 }
+	public static void UC2() {
+		Random r = new Random();
+		int randomInt = r.nextInt(6) + 1;
+		System.out.println("Die Number: " + randomInt);
+	}
 }
