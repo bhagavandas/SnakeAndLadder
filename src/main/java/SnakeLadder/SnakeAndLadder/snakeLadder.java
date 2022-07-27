3f11f2be79e28c1e2b63b45f867acc348397ad53
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-07-27 06:51:19 +0530

    Added: UC1

---
+++ src/main/java/SnakeLadder/SnakeAndLadder/snakeLadder.java
@@ -0,0 +1,14 @@
+package SnakeLadder.SnakeAndLadder;
+
+import java.util.Random;
+
+public class snakeLadder {
+	public static void main(String[] args) {
+		UC1();
+		
+	}
+	public static void UC1() {
+		int position =0;
+		System.out.println("Player starting Position : " + position);
+}
+}
