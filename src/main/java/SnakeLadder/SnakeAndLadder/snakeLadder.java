fc0f5207fec9c82d2b97aa3212f8606deb903bc7
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-07-27 09:08:41 +0530

    Added: UC5

--- src/main/java/SnakeLadder/SnakeAndLadder/snakeLadder.java
+++ src/main/java/SnakeLadder/SnakeAndLadder/snakeLadder.java
@@ -4,17 +4,33 @@ import java.util.Random;
 
 public class snakeLadder {
 	static int total=0;
+	static int total1=0;
 	static int cal=0;
 	static int count=0;
+	static int count1=0;
 
-	//  use function  to roll the die
+	static void player1() {
+		while (total < 100) {
+
+			int check = (int) Math.floor((Math.random() * 10) % 3);
+			checkPlayer(check);
+		}
+	}
+
+	
 	static void Roll() {
 		int min=1;
 		int max=6;
 		int Die_num = (int) Math.floor(Math.random()*(max-min)+min);
 
 		System.out.println("Die = "+Die_num);
+		System.out.println("Player 1 rolls the die");
+		System.out.println("Die = " + Die_num);
 
+		count = count + 1;  // Count how many times the Die was played
+
+		if (total + Die_num <= 100) {
+			total = total + Die_num;
 
 		if (total <= 100) {
 
@@ -74,6 +90,94 @@ public class snakeLadder {
 		else {
 			Roll();
 		}
+		}
+	}
+		static void player2() {
+			while (total < 100) {
+
+				int check = (int) Math.floor((Math.random() * 10) % 3);
+			
+			int minimum = 1;
+			int maximum = 6;
+			int dieNumber1 = (int) Math.floor(Math.random() * (maximum - minimum) + minimum);
+
+			System.out.println("Die = " + dieNumber1);
+
+			count1 = count1 + 1;
+
+			if (total1 + dieNumber1 <= 100) {
+				total1 = total1 + dieNumber1;
+				System.out.println("Player2 in the position = " + total1);
+				switch (total1) {
+					case 10:
+						System.out.println("Ladder");
+						total1 = total1 + 10;
+						System.out.println("After Ladder = " + total1);
+						player2();
+						break;
+					case 20:
+						System.out.println("Snake");
+						total1 = total1 - 5;
+						System.out.println("After Snake = " + total1);
+						player1();
+						break;
+					case 30:
+						System.out.println("Ladder");
+						total1 = total1 + 10;
+						System.out.println("After Ladder = " + total1);
+						player2();
+						break;
+					case 40:
+						System.out.println("Snake");
+						total1 = total1 - 5;
+						System.out.println("After Snake = " + total1);
+						player1();
+						break;
+					case 50:
+						System.out.println("Ladder");
+						total1 = total1 + 10;
+						System.out.println("After Ladder = " + total1);
+						player2();
+						break;
+					case 60:
+						System.out.println("Snake");
+						total1 = total1 - 5;
+						System.out.println("After Snake = " + total1);
+						player1();
+						break;
+					case 70:
+						System.out.println("Ladder");
+						total1 = total1 + 10;
+						System.out.println("After Ladder = " + total1);
+						player2();
+						break;
+					case 80:
+						System.out.println("Snake");
+						total1 = total1 - 5;
+						System.out.println("After Snake = " + total1);
+						player1();
+						break;
+					case 90:
+						System.out.println("Ladder");
+						total1 = total1 + 10;
+						System.out.println("After Ladder = " + total1);
+						player2();
+						break;
+					case 100:
+						System.out.println("Player in the position = " + total1);
+						System.out.println("Exit");
+						System.out.println(count1 + " times the dice was played to win the game");
+						break;
+					default:
+						player2();
+
+				}
+			
+		}
+			else {
+				player2();
+			}
+			}
 	}
 
 	static void checkPlayer(int check) {
